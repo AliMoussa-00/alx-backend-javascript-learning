28d276dd1c17c39a4a44eb03143dd84409c41e1e
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-04-17 15:49:46 +0100

    finishing task_1

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,3 +1,21 @@
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName[0], ". ").concat(lastName);
+};
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return 'Currently working';
+    };
+    StudentClass.prototype.displayName = function () {
+        return this._firstName;
+    };
+    return StudentClass;
+}());
+// /////////////////////////////testing /////////////////////////////////
+// ## task_0
 var teacherA = {
     firstName: 'Ali',
     lastName: 'Moussa',
@@ -14,5 +32,23 @@ var teacherB = {
     // adding custom attr
     isGoodAtJob: true,
 };
-console.log(teacherA);
-console.log(teacherB);
+// console.log(teacherA);
+// console.log(teacherB);
+// ## task_1
+var director = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+// console.log(director);
+// ## task_3
+// console.log(printTeacher('John', 'Doe'));
+// ## task_4
+function createStudent(constructor, firstName, lastName) {
+    return new constructor(firstName, lastName);
+}
+var student = createStudent(StudentClass, 'Ali', 'Moussa');
+console.log(student.displayName());
+console.log(student.workOnHomework());
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,60 @@ interface Teacher {
 	[attribute: string]: any;
 }
 
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+
+/**
+ * We define an interface named PrintTeacherFunction with a function signature
+ * that accepts two parameters of type string (firstName and lastName) and returns a string.
+ */
+interface PrintTeacher {
+	(firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacher = (firstName, lastName) => {
+	return `${firstName[0]}. ${lastName}`;
+};
+
+/**
+ * Defining the class through a constructor by
+ * describes the public properties and methods of the class
+ */
+interface StudentClassInterface {
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+/**
+ * Defining the constructor through an interface
+ */
+interface StudentClassConstructorInterface {
+	new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+	private _firstName: string;
+	private _lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this._firstName = firstName;
+		this._lastName = lastName;
+	}
+
+	workOnHomework() {
+		return 'Currently working';
+	}
+
+	displayName() {
+		return this._firstName;
+	}
+}
+
+
+// /////////////////////////////testing /////////////////////////////////
+
+// ## task_0
 const teacherA: Teacher = {
 	firstName: 'Ali',
 	lastName: 'Moussa',
@@ -25,5 +79,31 @@ const teacherB: Teacher = {
 	isGoodAtJob: true,
 };
 
-console.log(teacherA);
-console.log(teacherB);
+// console.log(teacherA);
+// console.log(teacherB);
+
+// ## task_1
+const director: Directors = {
+	firstName: 'John',
+	lastName: 'Doe',
+	location: 'London',
+	fullTimeEmployee: true,
+	numberOfReports: 17,
+};
+// console.log(director);
+
+// ## task_3
+// console.log(printTeacher('John', 'Doe'));
+
+// ## task_4
+function createStudent(
+	constructor: StudentClassConstructorInterface,
+	firstName: string,
+	lastName: string
+): StudentClassInterface {
+	return new constructor(firstName, lastName);
+}
+
+const student = createStudent(StudentClass, 'Ali', 'Moussa');
+console.log(student.displayName());
+console.log(student.workOnHomework());
